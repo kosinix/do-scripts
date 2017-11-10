b46cbb60877b8125e36b0e7f1d3ec9190a6f7553
Nico Amarilla <kosinix@users.noreply.github.com>
2017-11-10 18:41:06 +0800

    Remove dupe

--- apache-setup.sh
+++ apache-setup.sh
@@ -3,7 +3,6 @@ sudo a2enmod rewrite &&
 sudo apt-get install php-curl &&
 sudo apt-get install php-dom &&
 sudo apt-get install php-sqlite &&
-sudo apt-get install php-sqlite &&
 sudo apt-get install php-mcrypt &&
 sudo apt-get install php-zip &&
 sudo systemctl restart apache2 
